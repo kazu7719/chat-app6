2af65b2e95d968ea81bcb6d496b4110cf56db978
kazu7719 <kazuki.eripon7719@icloud.com>
2025-11-05 18:32:02 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :parameter_method, if: :devise_controller?
+
+  private
+  def parameter_method
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
