a91d2379a3e57ddc121e4939c91743db078eedce
kazu7719 <kazuki.eripon7719@icloud.com>
2025-11-06 15:29:42 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,11 +4,16 @@ class RoomsController < ApplicationController
   end
 
   def create
-    Room.create(room_params)
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def room_params
-    params.require(:room).permit(:name).merge(user_id)
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
